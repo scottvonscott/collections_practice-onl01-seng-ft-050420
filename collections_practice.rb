82ef25d43bd265709d5835d9a723b9f3d0b30d00
Scott  Keplinger <skeplin17@gmail.com>
2020-05-09 19:59:42 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -37,16 +37,8 @@ end
 
 def add_s (array)
   new_array = []
-  new_array = array.collect do |name|
-    next if array.index == 1
-  else
+  new_array = array.each_with_indexcollect do |name, index|
+    break if index == 1
     name.insert(-1, "s")
-  end
  new_array.insert(1, array[1])
 end
-
-# def add_s (array)
-#   new_array = []
-#  new_array = array.collect {|name| next if array.index == 1 name.insert(-1, "s")}
-#  new_array.insert(1, array[1])
-# end
