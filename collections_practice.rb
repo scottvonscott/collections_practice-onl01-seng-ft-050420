2218e48141e37f7e725b94559853ccac4eae315a
Scott  Keplinger <skeplin17@gmail.com>
2020-05-09 19:48:43 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -37,6 +37,6 @@ end
 
 def add_s (array)
   new_array = []
- new_array = array.reject(array[1]).collect {|name| name.insert(-1, "s")}
+ new_array = array.collect {|name| name.insert(-1, "s") if name.index != 1}
  new_array.insert(1, array[1])
 end
